dc013f9fee61c32b01c4249193d073325cf0a257
koosenmongol <koosenmongol@gmail.com>
2024-12-13 13:18:51 +0800

    data

--- instagram/src/data.tsx
+++ instagram/src/data.tsx
@@ -1,10 +1,136 @@
 export const postData = [
-  { id: 1, desc: "My post1", img: require("../assets/images/Post1.png") },
-  { id: 2, desc: "My post2", img: require("../assets/images/Post2.png") },
-  { id: 3, desc: "My post3", img: require("../assets/images/Post3.png") },
-  { id: 4, desc: "My post1", img: require("../assets/images/Post4.png") },
-  { id: 5, desc: "My post2", img: require("../assets/images/Post5.png") },
-  { id: 6, desc: "My post3", img: require("../assets/images/Post6.png") },
+  {
+    id: 1,
+    desc: "My post1",
+    img: require("../assets/images/Post1.png"),
+    comment: [
+      {
+        id: "User1",
+        idProfile: "https://randomuser.me/api/portraits/med/men/75.jpg",
+        text: "Comment1",
+      },
+      {
+        id: "User2",
+        idProfile: "https://randomuser.me/api/portraits/med/men/75.jpg",
+        text: "Comment2",
+      },
+      {
+        id: "User3",
+        idProfile: "https://randomuser.me/api/portraits/med/men/75.jpg",
+        text: "Comment3",
+      },
+    ],
+  },
+  {
+    id: 2,
+    desc: "My post2",
+    img: require("../assets/images/Post2.png"),
+    comment: [
+      {
+        id: "User1",
+        idProfile: "https://randomuser.me/api/portraits/med/men/75.jpg",
+        text: "Comment1",
+      },
+      {
+        id: "User2",
+        idProfile: "https://randomuser.me/api/portraits/med/men/75.jpg",
+        text: "Comment2",
+      },
+      {
+        id: "User3",
+        idProfile: "https://randomuser.me/api/portraits/med/men/75.jpg",
+        text: "Comment3",
+      },
+    ],
+  },
+  {
+    id: 3,
+    desc: "My post3",
+    img: require("../assets/images/Post3.png"),
+    comment: [
+      {
+        id: "User1",
+        idProfile: "https://randomuser.me/api/portraits/med/men/75.jpg",
+        text: "Comment1",
+      },
+      {
+        id: "User2",
+        idProfile: "https://randomuser.me/api/portraits/med/men/75.jpg",
+        text: "Comment2",
+      },
+      {
+        id: "User3",
+        idProfile: "https://randomuser.me/api/portraits/med/men/75.jpg",
+        text: "Comment3",
+      },
+    ],
+  },
+  {
+    id: 4,
+    desc: "My post1",
+    img: require("../assets/images/Post4.png"),
+    comment: [
+      {
+        id: "User1",
+        idProfile: "https://randomuser.me/api/portraits/med/men/75.jpg",
+        text: "Comment1",
+      },
+      {
+        id: "User2",
+        idProfile: "https://randomuser.me/api/portraits/med/men/75.jpg",
+        text: "Comment2",
+      },
+      {
+        id: "User3",
+        idProfile: "https://randomuser.me/api/portraits/med/men/75.jpg",
+        text: "Comment3",
+      },
+    ],
+  },
+  {
+    id: 5,
+    desc: "My post2",
+    img: require("../assets/images/Post5.png"),
+    comment: [
+      {
+        id: "User1",
+        idProfile: "https://randomuser.me/api/portraits/med/men/75.jpg",
+        text: "Comment1",
+      },
+      {
+        id: "User2",
+        idProfile: "https://randomuser.me/api/portraits/med/men/75.jpg",
+        text: "Comment2",
+      },
+      {
+        id: "User3",
+        idProfile: "https://randomuser.me/api/portraits/med/men/75.jpg",
+        text: "Comment3",
+      },
+    ],
+  },
+  {
+    id: 6,
+    desc: "My post3",
+    img: require("../assets/images/Post6.png"),
+    comment: [
+      {
+        id: "User1",
+        idProfile: "https://randomuser.me/api/portraits/med/men/75.jpg",
+        text: "Comment1",
+      },
+      {
+        id: "User2",
+        idProfile: "https://randomuser.me/api/portraits/med/men/75.jpg",
+        text: "Comment2",
+      },
+      {
+        id: "User3",
+        idProfile: "https://randomuser.me/api/portraits/med/men/75.jpg",
+        text: "Comment3",
+      },
+    ],
+  },
   { id: 7, desc: "My post1", img: require("../assets/images/Post1.png") },
   { id: 8, desc: "My post2", img: require("../assets/images/Post2.png") },
   { id: 9, desc: "My post3", img: require("../assets/images/Post3.png") },
